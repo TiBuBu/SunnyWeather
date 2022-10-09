bb123ec68b2fa365b628c6daf5222e4160853259
TiBuBu <2729680969@qq.com>
2022-10-09 16:43:54 +0800

    error

---
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -0,0 +1,122 @@
+package com.sunnyweather.android.ui.weather
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.sunnyweather.android.R
+import com.sunnyweather.android.logic.model.Weather
+import com.sunnyweather.android.logic.model.getSky
+import com.sunnyweather.android.ui.place.WeatherViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+class WeatherActivity : AppCompatActivity() {
+    val viewModel by lazy {ViewModelProvider(this).get(WeatherViewModel::class.java)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+        if(viewModel.locationLng.isEmpty()){
+            viewModel.locationLng = intent.getStringExtra("location_lng")?:""
+        }
+        if(viewModel.locationLat.isEmpty()){
+            viewModel.locationLat = intent.getStringExtra("location_lat")?:""
+        }
+        if(viewModel.placeName.isEmpty()){
+            viewModel.placeName = intent.getStringExtra("place_name")?:""
+        }
+
+        viewModel.weatherLiveData.observe(this, Observer {
+            result->
+            val swipeRefresh:SwipeRefreshLayout = findViewById(R.id.swipeRefresh)
+            val weather = result.getOrNull()
+            if(weather != null){
+                showWeatherInfo(weather)
+            }else{
+                Toast.makeText(this,"无法成功获取天气信息",Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+            swipeRefresh.isRefreshing = false
+        })
+        refreshWeather()
+        val swipeRefresh:SwipeRefreshLayout = findViewById(R.id.swipeRefresh)
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+        val navBtn:Button = findViewById(R.id.navBtn)
+        val drawerLayout:DrawerLayout = findViewById(R.id.drawerLayout)
+        navBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        drawerLayout.addDrawerListener(object :DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(newState: Int) {}
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+            override fun onDrawerOpened(drawerView: View) {}
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken,InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
+
+    }
+    fun refreshWeather(){
+        viewModel.refreshWeather(viewModel.locationLng,viewModel.locationLat)
+        val swipeRefresh:SwipeRefreshLayout = findViewById(R.id.swipeRefresh)
+        swipeRefresh.isRefreshing = true
+    }
+    private fun showWeatherInfo(weather: Weather){
+        val placeName:TextView = findViewById(R.id.placeName)
+        placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+        val currentTempText = "${realtime.temperature.toInt()} °C"
+        val currentTemp:TextView = findViewById(R.id.currentTemp)
+        currentTemp.text = currentTempText
+        val currentSky:TextView = findViewById(R.id.currentSky)
+        currentSky.text = getSky(realtime.skycon).info
+        val currentPM25Text = "空气指数 ${realtime.airQuality.aqi.chn.toInt()}"
+        val currentAQI:TextView = findViewById(R.id.currentAQI)
+        currentAQI.text = currentPM25Text
+        val nowLayout: RelativeLayout = findViewById(R.id.nowLayout)
+        nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
+        val forecastLayout: LinearLayout = findViewById(R.id.forecastLayout)
+        forecastLayout.removeAllViews()
+        val days = daily.skycon.size
+        for (i in 0 until days){
+            val skycon = daily.skycon[i]
+            val temperature = daily.temperature[i]
+            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item,forecastLayout,false)
+            val dateInfo = view.findViewById(R.id.dateInfo) as TextView
+            val skyIcon = view.findViewById<ImageView>(R.id.skyIcon)
+            val skyInfo = view.findViewById<TextView>(R.id.skyInfo)
+            val temperatureInfo = view.findViewById<TextView>(R.id.temperatureInfo)
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            dateInfo.text = simpleDateFormat.format(skycon.date)
+            val sky = getSky(skycon.value)
+            skyIcon.setImageResource(sky.icon)
+            skyInfo.text = sky.info
+            val tempText = "${temperature.min.toInt()} ~ ${temperature.max.toInt()} °C"
+            temperatureInfo.text = tempText
+            forecastLayout.addView(view)
+        }
+        val lifeIndex = daily.lifeIndex
+        val coldRiskText:TextView = findViewById(R.id.coldRiskText)
+        val dressingText:TextView = findViewById(R.id.dressingText)
+        val ultravioletText:TextView = findViewById(R.id.ultravioletText)
+        val carWashingText:TextView = findViewById(R.id.carWashingText)
+        coldRiskText.text = lifeIndex.coldRisk[0].desc
+        dressingText.text = lifeIndex.dressing[0].desc
+        ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        carWashingText.text = lifeIndex.carWashing[0].desc
+        val weatherLayout: ScrollView = findViewById(R.id.weatherLayout)
+        weatherLayout.visibility = View.VISIBLE
+    }
+}
